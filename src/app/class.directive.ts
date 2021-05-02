89d812a8a7947b72f5c0a1b87d4cf03d0a32ca3d
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-05-02 01:01:50 -0700

    custom directive example

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  ElementRef
+} from '@angular/core';
 
 @Directive({
   selector: '[appClass]',
 })
 export class ClassDirective {
-  constructor() {
+  constructor(private element: ElementRef) {
     console.log('class directive is being used');
+    this.element.nativeElement.style.backgroundColor = 'deeppink';
   }
 }
+
