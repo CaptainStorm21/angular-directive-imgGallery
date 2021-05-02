ccda90598b1af4d617d574cd9c97ecc9f69dc981
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-05-01 19:05:40 -0700

    images added to the object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-directive-imgGallery';
+  images = [
+    {
+      title: 'Landscape',
+      url: 'https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg'
+    },
+    {
+      title: 'Morning Landscape',
+      url: 'https://www.mickeyshannon.com/photos/zoom/mountain-photography.jpg'
+    },
+    {
+      title: 'Mountains and Sky',
+      url: 'https://www.mickeyshannon.com/photos/tunnel-view-sunset.jpg'
+    },
+    {
+      title: 'Sunset',
+      url: 'https://www.mickeyshannon.com/photos/cosmic-grand-teton.jpg'
+    },
+    {
+      title: 'Birch',
+      url: 'https://www.mickeyshannon.com/photos/zoom/aspen-alley-panorama.jpg'
+    },
+    {
+      title: 'Interior Design',
+      url: 'https://www.mickeyshannon.com/images/large/interior-design-for-business.jpg'
+    },
+  ]
 }
