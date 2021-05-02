f37cccbc87232d5e753e2709ae0929fd992e907d
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-05-02 01:51:43 -0700

    custom directive

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,15 +1,39 @@
 import {
   Directive,
-  ElementRef
+  ElementRef,
+  Input
 } from '@angular/core';
 
 @Directive({
   selector: '[appClass]',
 })
 export class ClassDirective {
+
+ @Input() backgroundColor: string;
+
   constructor(private element: ElementRef) {
     console.log('class directive is being used');
+    this.element.nativeElement.style.color = 'deeppink';
     this.element.nativeElement.style.backgroundColor = 'deeppink';
   }
+
+
 }
 
+
+/*
+Communicate from parent to child support
+
+Parent component template
+<app-card 
+         [title]" 'SnowyMountain' "></app-card>
+*/
+
+// Child component class
+/*
+import { Input } from 'angular';
+
+calss ChildComponent {
+  @Input() title: string; 
+}
+*/
