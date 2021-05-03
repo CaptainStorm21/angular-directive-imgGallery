436ef25fdaf790bdfc650c05e33a3642c2ea42ad
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-05-02 22:41:53 -0700

    building custom ngDirective

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -10,13 +10,20 @@ import {
 export class ClassDirective {
 
 //  @Input() backgroundColor: string;
-
   constructor(private element: ElementRef) {
     console.log('class directive is being used');
     this.element.nativeElement.style.color = 'deeppink';
   }
 
-  @Input() set backgroundColor (color: string) {
+  // @Input() set backgroundColor (color: string) {
+  //   this.element.nativeElement.style.backgroundColor = color;
+  // }
+
+  // @Input() set appClass (color: string) {
+  //   this.element.nativeElement.style.backgroundColor = color;
+  // }
+
+  @Input('appClass') set backgroundColor (color: string) {
     this.element.nativeElement.style.backgroundColor = color;
   }
 
